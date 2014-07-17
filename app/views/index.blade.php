29940ea4a2ba68bb3de583c7345c9c095b658a8a
Manish Goyal <manishgo81@gmail.com>
2014-07-17 15:30:20 -0400

    Validator error

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html >
+<html lang="en">
 	<head>
 		<LINK href="Styles/Style2.css" rel="stylesheet" type="text/css">
 		<meta charset="UTF-8">
@@ -10,12 +10,12 @@
 		<div>	
 			<h1 class="Heading1">Developer's Best Friend</h1><br/><br/><br/><br/>
 			<div class="MainBox3">
-				<a href='/LIG'><font size="5">Lorem Ipsum Text Generator</font></a> &nbsp
-				<a class="help" href='http://en.wikipedia.org/wiki/Lorem_ipsum'><font size="5">?</font></a>
+				<a href='/LIG'><font size="5">Lorem Ipsum Text Generator</font></a> &nbsp;
+				<a class="help" href='http://en.wikipedia.org/wiki/Lorem_ipsum'><font size="6">?</font></a>
 			</div>
 			<h4><p class="Center2"> In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. 
 					Replacing meaningful content that could be distracting with placeholder text may allow viewers to focus on graphic aspects such as font, typography, and page layout. (Src: Wikipedia)</p></h4>
-			</br></br><br/>
+			<br/><br/><br/>
 			<div class="MainBox3">
 				<a href='/RUG'><font size="5">Random User Generator</font></a>
 			</div>
