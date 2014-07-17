59e1a7659e68fc892edee3d87ec99b068def56f2
Manish Goyal <manishgo81@gmail.com>
2014-07-17 15:51:50 -0400

    Validator errors

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -13,17 +13,17 @@
 				<a class="link" href='/LIG'> Lorem Ipsum Text Generator </a> &nbsp;
 				<a class="help" href='http://en.wikipedia.org/wiki/Lorem_ipsum'>?</a>
 			</div>
-			<h4><p class="Center2"> In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. 
-					Replacing meaningful content that could be distracting with placeholder text may allow viewers to focus on graphic aspects such as font, typography, and page layout. (Src: Wikipedia)</p></h4>
+			<h4 class="Center2">In publishing and graphic design, lorem ipsum is a filler text commonly used to demonstrate the graphic elements of a document or visual presentation. 
+					Replacing meaningful content that could be distracting with placeholder text may allow viewers to focus on graphic aspects such as font, typography, and page layout. (Src: Wikipedia)</h4>
 			<br/><br/><br/>
 			<div class="MainBox3">
 				<a class="link" href='/RUG'>Random User Generator</a>
 			</div>
-			<h3><p class="Center3">Generates Random User Data (Name, DOB, Address, Text)</p></h3><br/><br/><br/>
+			<h3 class="Center3">Generates Random User Data (Name, DOB, Address, Text)</h3><br/><br/><br/>
 			<div class="MainBox3">
 				<a class="link" href='/RPG'>Random Password Generator</a>
 			</div>
-			<h3><p class="Center4">Generates Random Password</p></h3><br/><br/><br/>
+			<h3 class="Center4">Generates Random Password</h3><br/><br/><br/>
 		</div>
 	</body>
 
